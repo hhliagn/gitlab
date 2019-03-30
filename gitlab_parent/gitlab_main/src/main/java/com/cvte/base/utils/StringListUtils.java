b168b5ab4a9be11fa5f92810f3560d6ec8d91ff7
lhh <s_lianghenghui@cvte.com>
2019-03-30 13:37:26 +0800

    modify StringList method use merge algorithmn

--- gitlab_parent/gitlab_main/src/main/java/com/cvte/base/utils/StringListUtils.java
+++ gitlab_parent/gitlab_main/src/main/java/com/cvte/base/utils/StringListUtils.java
@@ -1,10 +1,90 @@
 package com.cvte.base.utils;
 
+import org.apache.logging.log4j.util.PropertySource;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class StringListUtils {
 
-    public static String compareCommitList(String diff1,String diff2) {
+    //归并算法提取两个字符串差异数据
+    public static String compareCommitList(String diff1,String diff2) throws Exception {
+
+        String[] diffList1 = diff1.split("\\n");
+        String[] diffList2 = diff2.split("\\n");
+
+        String[] combine=new String[diffList1.length+diffList2.length];
+        for (int i=0;i<diffList1.length;i++){
+            combine[i]=diffList1[i];
+        }
+        int temp=diffList1.length;
+        for (int i=0;i<diffList2.length;i++){
+            combine[temp+i]=diffList2[i];
+        }
+
+        List<String> a=new ArrayList<>();
+        List<String> b=new ArrayList<>();
+
+        int p=0;
+        int q=temp;
+        while (p<temp||q<combine.length){
+
+            if (p>=temp){
+                b.add(combine[q]);
+                q++;
+            }
+            if (q>=combine.length){
+                a.add(combine[p]);
+                p++;
+            }
+
+            int cmp = combine[p].compareTo(combine[q]);
+            if (cmp!=0){
+                a.add(combine[p]);
+                b.add(combine[q]);
+            }
+            p++;
+            q++;
+
+            /*if (cmp>0){
+                b.add(combine[q+1]);
+                q++;
+            }*/
+        }
+
+        File file = new File("diff.txt");
+        FileOutputStream fos = new FileOutputStream(file);
+
+        fos.write("commit1:".getBytes());
+        for (String s : a) {
+
+            fos.write(s.getBytes());
+            fos.write("  ".getBytes());
+        }
+
+        fos.write(";".getBytes());
+
+        fos.write("commit2:".getBytes());
+        for (String s : b) {
+
+            fos.write(s.getBytes());
+            fos.write("  ".getBytes());
+        }
+
+        fos.flush();
+        fos.close();
+        return "OK";
+    }
+
+
+
+
+
+    /*public static String compareCommitList(String diff1,String diff2) {
 
         String[] diffList1 = diff1.split("\\n");
         String[] diffList2 = diff2.split("\\n");
@@ -12,12 +92,12 @@ public class StringListUtils {
         StringBuilder sb=new StringBuilder();
 
         //master 不往前走
-        /*int shortLength=diffList1.length;
+        *//*int shortLength=diffList1.length;
         int dist=diffList2.length-diffList1.length;
         for (int i=0;i<dist;i++){
             sb.append(diffList2[i + shortLength]+" ");
         }
-        return sb.toString();*/
+        return sb.toString();*//*
 
         //master 一直更新
 
@@ -93,5 +173,5 @@ public class StringListUtils {
 
         String diff = sb.toString().replaceAll("\\\\ No newline at end of file", "");
         return diff;
-    }
+    }*/
 }
